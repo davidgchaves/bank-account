ff18e164b6ffcbc8ddaec77095468ff600d96ac1
David G Chaves <davidgchaves@gmail.com>
2014-05-09 17:34:42 +0200

    Pass test 'Account starts off with a balance of 0'

--- lib/bank_account.ex
+++ lib/bank_account.ex
@@ -6,4 +6,19 @@ defmodule BankAccount do
   def start(_type, _args) do
     BankAccount.Supervisor.start_link
   end
+
+  def start do
+    await
+  end
+
+  def await do
+    receive do
+      {:check_balance, pid} -> divulge_balance pid
+    end
+    await
+  end
+
+  defp divulge_balance(pid) do
+    send pid, {:balance, 0}
+  end
 end
--- test/bank_account_test.exs
+++ test/bank_account_test.exs
@@ -1,7 +1,9 @@
 defmodule BankAccountTest do
   use ExUnit.Case
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  test "starts off with a balance of 0" do
+    account = spawn_link BankAccount, :start, []
+    send account, {:check_balance, self}
+    assert_receive {:balance, 0}
   end
 end
