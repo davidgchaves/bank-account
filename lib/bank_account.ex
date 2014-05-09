6a6a1b126aaebce590dc12d93163df5d09a9eb2c
David G Chaves <davidgchaves@gmail.com>
2014-05-09 19:13:18 +0200

    Pass test 'Account has balance decremented by the amount of a withdrawal'

--- lib/bank_account.ex
+++ lib/bank_account.ex
@@ -15,20 +15,45 @@ defmodule BankAccount do
     receive do
       {:check_balance, pid} -> divulge_balance pid, events
       {:deposit, amount}    -> events = deposit amount, events
+      {:withdraw, amount}   -> events = withdraw amount, events
     end
     await events
-  end
-
-  defp deposit(amount, events) do
-    events ++ [{:deposit, amount}]
   end
 
   defp divulge_balance(pid, events) do
     send pid, {:balance, calculate_balance events}
   end
 
+  defp deposit(amount, events) do
+    events ++ [{:deposit, amount}]
+  end
+
+  defp withdraw(amount, events) do
+    events ++ [{:withdrawal, amount}]
+  end
+
   defp calculate_balance(events) do
-    sum events
+    sum_deposits(events) - sum_withdrawals(events)
+  end
+
+  defp sum_deposits(events) do
+    sum just_deposits events
+  end
+
+  defp sum_withdrawals(events) do
+    sum just_withdrawals events
+  end
+
+  defp just_deposits(events) do
+    just_type events, :deposit
+  end
+
+  defp just_withdrawals(events) do
+    just_type events, :withdrawal
+  end
+
+  defp just_type(events, expected_type) do
+    Enum.filter events, fn({type, _}) -> type == expected_type end
   end
 
   defp sum(events) do
--- test/bank_account_test.exs
+++ test/bank_account_test.exs
@@ -3,14 +3,24 @@ defmodule BankAccountTest do
 
   test "starts off with a balance of 0" do
     account = spawn_link BankAccount, :start, []
-    send account, {:check_balance, self}
-    assert_receive {:balance, 0}
+    verify_balance_is 0, account
   end
 
   test "has balance incremented by the amount of a deposit" do
     account = spawn_link BankAccount, :start, []
     send account, {:deposit, 100}
-    send account, {:check_balance, self}
-    assert_receive {:balance, 100}
+    verify_balance_is 100, account
+  end
+
+  test "has balance decremented by the amount of a withdrawal" do
+    account = spawn_link BankAccount, :start, []
+    send account, {:deposit, 150}
+    send account, {:withdraw, 100}
+    verify_balance_is 50, account
+  end
+
+  defp verify_balance_is(expected_balance, actual_account) do
+    send actual_account, {:check_balance, self}
+    assert_receive {:balance, ^expected_balance}
   end
 end
