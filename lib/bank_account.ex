b79a10003b3eb4a5477494d35626be94042220c2
David G Chaves <davidgchaves@gmail.com>
2014-05-09 17:59:04 +0200

    Pass test 'Account has balance incremented by the amount of a deposit'

--- lib/bank_account.ex
+++ lib/bank_account.ex
@@ -8,17 +8,30 @@ defmodule BankAccount do
   end
 
   def start do
-    await
+    await []
   end
 
-  def await do
+  def await(events) do
     receive do
-      {:check_balance, pid} -> divulge_balance pid
+      {:check_balance, pid} -> divulge_balance pid, events
+      {:deposit, amount}    -> events = deposit amount, events
     end
-    await
+    await events
   end
 
-  defp divulge_balance(pid) do
-    send pid, {:balance, 0}
+  defp deposit(amount, events) do
+    events ++ [{:deposit, amount}]
+  end
+
+  defp divulge_balance(pid, events) do
+    send pid, {:balance, calculate_balance events}
+  end
+
+  defp calculate_balance(events) do
+    sum events
+  end
+
+  defp sum(events) do
+    Enum.reduce events, 0, fn({_, amount}, acc) -> acc + amount end
   end
 end
--- test/bank_account_test.exs
+++ test/bank_account_test.exs
@@ -6,4 +6,11 @@ defmodule BankAccountTest do
     send account, {:check_balance, self}
     assert_receive {:balance, 0}
   end
+
+  test "has balance incremented by the amount of a deposit" do
+    account = spawn_link BankAccount, :start, []
+    send account, {:deposit, 100}
+    send account, {:check_balance, self}
+    assert_receive {:balance, 100}
+  end
 end
